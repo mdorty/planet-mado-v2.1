abd54c696c902381bf9798b18443ac73cf2092c2
mdorty <mattdorty@gmail.com>
2025-04-15 09:04:27 -0500

    update nextauth url

--- lib/trpc/utils.ts
+++ lib/trpc/utils.ts
@@ -2,5 +2,6 @@ export function getUrl() {
     if (typeof window !== 'undefined') {
       return window.location.origin + '/api/trpc';
     }
-    return process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/trpc';
+    // For server-side, use NEXTAUTH_URL if available (important for production), otherwise fallback to a default
+    return process.env.NEXTAUTH_URL ? process.env.NEXTAUTH_URL + '/api/trpc' : (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/trpc');
   }
