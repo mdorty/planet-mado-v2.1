5e36228c87f1228f245c4527c4711f284bd7acff
mdorty <mattdorty@gmail.com>
2025-04-19 16:20:23 -0500

    socketio install

--- app/api/socket/route.ts
+++ app/api/socket/route.ts
@@ -5,11 +5,6 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
 
 let io: IOServer | null = null;
 
